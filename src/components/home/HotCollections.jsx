72a5c70d09e0e223cbe2ff5c105d669f71e080c0
Jennifer Cobb <jennifercobb829@gmail.com>
2026-06-07 09:36:46 -0400

    hot-collections&new-items

--- src/components/home/HotCollections.jsx
+++ src/components/home/HotCollections.jsx
@@ -1,13 +1,30 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import OwlCarousel from "react-owl-carousel";
-
-import "owl.carousel/dist/assets/owl.carousel.css";
-import "owl.carousel/dist/assets/owl.theme.default.css";
+import { useKeenSlider } from "keen-slider/react";
+import "keen-slider/keen-slider.min.css";
 
 const HotCollections = () => {
   const [collections, setCollections] = useState([]);
   const [loading, setLoading] = useState(true);
+
+  const [sliderRef] = useKeenSlider({
+    loop: true,
+    slides: {
+      perView: 4,
+      spacing: 20,
+    },
+    breakpoints: {
+      "(max-width: 1200px)": {
+        slides: { perView: 3, spacing: 15 },
+      },
+      "(max-width: 768px)": {
+        slides: { perView: 2, spacing: 15 },
+      },
+      "(max-width: 576px)": {
+        slides: { perView: 1, spacing: 10 },
+      },
+    },
+  });
 
   useEffect(() => {
     fetch(
@@ -27,26 +44,11 @@ const HotCollections = () => {
   if (loading) {
     return <div>Loading collections...</div>;
   }
-  const options = {
-    loop: true,
-    margin: 20,
-    nav: false,
-    dots: false,
-    autoplay: true,
-    autoplayTimeout: 3000,
-    smartSpeed: 1000,
-    autoplayHoverPause: true,
-    responsive: {
-      0: { items: 1 },
-      576: { items: 2 },
-      768: { items: 3 },
-      1200: { items: 4 },
-    },
-  };
 
   return (
     <section id="section-collections" className="no-bottom">
       <div className="container">
+        {/* Title */}
         <div className="row">
           <div className="col-lg-12 text-center">
             <h2>Hot Collections</h2>
@@ -54,44 +56,41 @@ const HotCollections = () => {
           </div>
         </div>
 
-        <div className="row">
-          <div className="col-lg-12">
-            <OwlCarousel className="owl-theme" {...options}>
-              {collections.map((item, index) => (
-                <div className="item" key={item.nftId || index}>
-                  <div className="nft_coll">
-                    <div className="nft_wrap">
-                      <Link to={`/item-details/${item.nftId}`}>
-                        <img
-                          src={item.nftImage}
-                          className="lazy img-fluid"
-                          alt={item.title}
-                        />
-                      </Link>
-                    </div>
-
-                    <div className="nft_coll_pp">
-                      <Link to="/author">
-                        <img
-                          className="lazy pp-coll"
-                          src={item.authorImage}
-                          alt={item.title}
-                        />
-                      </Link>
-                      <i className="fa fa-check"></i>
-                    </div>
-
-                    <div className="nft_coll_info">
-                      <Link to="/explore">
-                        <h4>{item.title}</h4>
-                      </Link>
-                      <span>{item.nftId}</span>
-                    </div>
-                  </div>
+        {/* Slider */}
+        <div ref={sliderRef} className="keen-slider">
+          {collections.map((item, index) => (
+            <div className="keen-slider__slide" key={item.nftId || index}>
+              <div className="nft_coll">
+                <div className="nft_wrap">
+                  <Link to={`/item-details/${item.nftId}`}>
+                    <img
+                      src={item.nftImage}
+                      className="img-fluid"
+                      alt={item.title}
+                    />
+                  </Link>
                 </div>
-              ))}
-            </OwlCarousel>
-          </div>
+
+                <div className="nft_coll_pp">
+                  <Link to="/author">
+                    <img
+                      className="pp-coll"
+                      src={item.authorImage}
+                      alt={item.title}
+                    />
+                  </Link>
+                  <i className="fa fa-check"></i>
+                </div>
+
+                <div className="nft_coll_info">
+                  <Link to="/explore">
+                    <h4>{item.title}</h4>
+                  </Link>
+                  <span>{item.nftId}</span>
+                </div>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </section>
--- src/components/home/NewItems.jsx
+++ src/components/home/NewItems.jsx
@@ -1,6 +1,30 @@
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { useKeenSlider } from "keen-slider/react";
+import "keen-slider/keen-slider.min.css";
+
 const NewItems = () => {
   const [items, setItems] = useState([]);
   const [loading, setLoading] = useState(true);
+
+  const [sliderRef] = useKeenSlider({
+    loop: true,
+    slides: {
+      perView: 4,
+      spacing: 20,
+    },
+    breakpoints: {
+      "(max-width: 1200px)": {
+        slides: { perView: 3, spacing: 15 },
+      },
+      "(max-width: 768px)": {
+        slides: { perView: 2, spacing: 15 },
+      },
+      "(max-width: 576px)": {
+        slides: { perView: 1, spacing: 10 },
+      },
+    },
+  });
 
   useEffect(() => {
     fetch("https://us-central1-nft-cloud-functions.cloudfunctions.net/newItems")
@@ -10,14 +34,12 @@ const NewItems = () => {
         setLoading(false);
       })
       .catch((err) => {
-        console.error("Error fetching new items:", err);
+        console.error(err);
         setLoading(false);
       });
   }, []);
 
-  if (loading) {
-    return <div>Loading new items...</div>;
-  }
+  if (loading) return <div>Loading new items...</div>;
 
   return (
     <section id="section-items" className="no-bottom">
@@ -30,53 +52,40 @@ const NewItems = () => {
           </div>
         </div>
 
-        {/* Items */}
-        <div className="row">
+        {/* Slider */}
+        <div ref={sliderRef} className="keen-slider">
           {items.map((item, index) => (
-            <div
-              className="col-lg-3 col-md-6 col-sm-6 col-xs-12"
-              key={item.nftId || index}
-            >
+            <div className="keen-slider__slide" key={item.nftId || index}>
               <div className="nft__item">
-                {/* Author */}
                 <div className="author_list_pp">
                   <Link to="/author">
-                    <img
-                      className="lazy"
-                      src={item.authorImage}
-                      alt={item.title}
-                    />
+                    <img src={item.authorImage} alt={item.title} />
                     <i className="fa fa-check"></i>
                   </Link>
                 </div>
 
-                {/* Countdown (static for now unless API has it) */}
                 <div className="de_countdown">5h 30m 32s</div>
 
-                {/* Image */}
                 <div className="nft__item_wrap">
                   <Link to={`/item-details/${item.nftId}`}>
                     <img
                       src={item.nftImage}
-                      className="lazy nft__item_preview"
+                      className="nft__item_preview"
                       alt={item.title}
                     />
                   </Link>
                 </div>
 
-                {/* Info */}
                 <div className="nft__item_info">
                   <Link to={`/item-details/${item.nftId}`}>
                     <h4>{item.title}</h4>
                   </Link>
 
-                  <div className="nft__item_price">
-                    {item.price || "3.08 ETH"}
-                  </div>
+                  <div className="nft__item_price">3.08 ETH</div>
 
                   <div className="nft__item_like">
                     <i className="fa fa-heart"></i>
-                    <span>{item.likes || 69}</span>
+                    <span>69</span>
                   </div>
                 </div>
               </div>
