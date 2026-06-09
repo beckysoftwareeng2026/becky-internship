484f7512b3144b44e0d460caaf3b621475830c5c
Jennifer Cobb <jennifercobb829@gmail.com>
2026-06-08 21:14:59 -0400

    updateexplore

--- src/components/explore/ExploreItems.jsx
+++ src/components/explore/ExploreItems.jsx
@@ -5,108 +5,66 @@ const ExploreItems = () => {
   const [items, setItems] = useState([]);
   const [loading, setLoading] = useState(true);
 
-  const ExploreItems = () => {
-    const [items, setItems] = useState([]);
+  useEffect(() => {
+    fetch("https://us-central1-nft-cloud-functions.cloudfunctions.net/explore")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log("EXPLORE API DATA:", data);
+        setItems(data.nfts || data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.error("API Error:", err);
+        setLoading(false);
+      });
+  }, []);
 
-    async function getItems() {
-      const response = await fetch(
-        "https://us-central1-nft-cloud-functions.cloudfunctions.net/explore",
-      );
-      const data = await response.json();
-      setItems(data);
-    }
-
-    useEffect(() => {
-      getItems();
-    }, []);
-
-    return (
-      <>
-        {/* filters here */}
-
-        {items.map((item) => (
-          <div
-            key={item.id}
-            className="d-item col-lg-3 col-md-6 col-sm-6 col-xs-12"
-            style={{ display: "block", backgroundSize: "cover" }}
-          >
-            {/* item card here */}
-          </div>
-        ))}
-      </>
-    );
-  };
+  if (loading) {
+    return <h3 className="text-center">Loading...</h3>;
+  }
 
   return (
-    <>
-      {/* Sort Dropdown */}
-      <div className="col-md-12">
-        <div className="items_filter mb30">
-          <form className="row">
-            <div className="col-md-3">
-              <select className="form-control">
-                <option>Default</option>
-                <option>Price, Low to High</option>
-                <option>Price, High to Low</option>
-                <option>Most Liked</option>
-              </select>
-            </div>
-          </form>
-        </div>
-      </div>
-
-      {/* NFT Cards */}
+    <div className="row">
       {items.map((item) => (
-        <div
-          key={item.nftId}
-          className="d-item col-lg-3 col-md-6 col-sm-6 col-xs-12 mb30"
-        >
+        <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={item.id}>
           <div className="nft__item">
             <div className="author_list_pp">
               <Link to="/author">
-                <img src={item.authorImage} alt="author" className="lazy" />
+                <img className="lazy" src={item.authorImage} alt="" />
                 <i className="fa fa-check"></i>
               </Link>
             </div>
 
-            <div className="de_countdown">{item.expiry || "5h 30m 32s"}</div>
-
             <div className="nft__item_wrap">
-              <div className="nft__item_extra">
-                <div className="nft__item_buttons">
-                  <button>Buy Now</button>
-                </div>
-              </div>
-
-              <Link to={`/item-details/${item.nftId}`}>
+              <Link to="/item-details">
                 <img
                   src={item.nftImage}
                   className="lazy nft__item_preview"
-                  alt={item.title}
+                  alt=""
                 />
               </Link>
             </div>
 
             <div className="nft__item_info">
-              <Link to={`/item-details/${item.nftId}`}>
+              <Link to="/item-details">
                 <h4>{item.title}</h4>
               </Link>
 
-              <div className="nft__item_price">{item.price || "3.08 ETH"}</div>
+              <div className="nft__item_price">{item.price} ETH</div>
+
+              <div className="nft__item_action">
+                <Link to="/item-details">Place a bid</Link>
+              </div>
 
               <div className="nft__item_like">
                 <i className="fa fa-heart"></i>
-                <span>{item.likes || 69}</span>
+                <span>{item.likes}</span>
               </div>
             </div>
           </div>
         </div>
       ))}
-
-      <div className="col-md-12 text-center mt-4">
-        <button className="btn-main lead">Load More</button>
-      </div>
-    </>
+    </div>
   );
 };
 
--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -1,57 +1,23 @@
-import React, { useEffect } from "react";
-import HeaderExplore from "../components/explore/HeaderExplore";
+import React from "react";
 import ExploreItems from "../components/explore/ExploreItems";
 
 const Explore = () => {
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, []);
-
   return (
-    <div id="wrapper">
-      <div className="no-bottom no-top" id="content">
-        {/* Hero Banner */}
-        <section
-          id="subheader"
-          className="text-light"
-          style={{
-            background: "url('/img/background/subheader.jpg') center center",
-            backgroundSize: "cover",
-            padding: "120px 0",
-          }}
-        >
-          <div className="center-y text-center">
-            <div className="container">
-              <div className="row">
-                <div className="col-md-12">
-                  <h1>Explore</h1>
-                </div>
-              </div>
-            </div>
-          </div>
-        </section>
-
-        {/* Filter Section */}
-        <section aria-label="section">
+    <>
+      <section id="subheader" className="text-light">
+        <div className="center-y relative text-center">
           <div className="container">
-            <div className="row">
-              <div className="col-md-12">
-                <HeaderExplore />
-              </div>
-            </div>
+            <h1>Explore</h1>
           </div>
-        </section>
+        </div>
+      </section>
 
-        {/* NFT Grid */}
-        <section aria-label="section">
-          <div className="container">
-            <div className="row">
-              <ExploreItems />
-            </div>
-          </div>
-        </section>
-      </div>
-    </div>
+      <section aria-label="section">
+        <div className="container">
+          <ExploreItems />
+        </div>
+      </section>
+    </>
   );
 };
 
